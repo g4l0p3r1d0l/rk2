5d8a7bd1a011ec53b400eda852271d5729723ec8
g4l0p3r1d0l <160529308+g4l0p3r1d0l@users.noreply.github.com>
2026-05-26 09:57:18 +0300

    Created test_command.cpp

---
+++ tests/test_command.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include "../webpage_button.cpp"
+
+TEST(CommandPatternTest, ChangeWebPageCommandExecutesChangeWebPage) {
+    std::stringstream buffer;
+    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+
+    WebPageManager manager;
+    ChangeWebPageCommand cmd(manager, "http://example.com");
+    cmd.Execute();
+
+    std::cout.rdbuf(old);
+    std::string output = buffer.str();
+    EXPECT_NE(output.find("Change web page to http://example.com."), std::string::npos);
+}
